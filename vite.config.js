88547f52f6210088f473ba92d3bd3dbf8632084e
wilsontt <wilsontt@icloud.com>
2025-12-28 23:01:25 +0800

    修改vite設定

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // 產品、開發中 的路徑䛀定
+  // ? 的後面是 正式環境(reace-week1)，
+  // ： 的後面是 開發中環境。
+  base: process.env.NODE_ENV === 'production' ? '/reace-week1/' : '/',
   plugins: [react()],
 })
